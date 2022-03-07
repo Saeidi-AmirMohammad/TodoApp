07fe2521bc8379d0895e7e26bf53ec0802d7a19a
SaeidiAmirMohammad <amirmohammad1379.saeidi@gmail.com>
2022-03-07 17:34:39 +0330

    Header Component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,77 @@
 import { PureComponent } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends PureComponent{
-render(){
-    return(
-        <a href="#">Saeidi AmirMohammad</a>
-    )
-}
+// Import Components
+import Header from "./Header";
+
+class App extends PureComponent {
+    render() {
+        return (
+            <div class="App">
+                <Header />
+                <main>
+                    <section class="jumbotron bg-light py-5">
+                        <div class="container d-flex flex-column align-items-center">
+                            <h1 class="jumbotron-heading">Welcome!</h1>
+                            <p class="lead text-muted">To get started, add some items to your list:</p>
+                            <div class="form-inline">
+                                <div class="form-group d-flex">
+                                    <input type="text" class="form-control mx-sm-3" placeholder="i want to do ..." />
+                                    <button class="btn btn-primary">add</button>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <div class="todosList">
+                        <div class="container">
+                            <div class="d-flex flex-column align-items-center ">
+                                <nav class="col-6 mb-3">
+                                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                                        <a class="nav-item nav-link active font-weight-bold" id="nav-home-tab">undone <span class="badge bg-secondary">9</span></a>
+                                        <a class="nav-item nav-link font-weight-bold" id="nav-profile-tab">done <span class="badge bg-success">9</span></a>
+                                    </div>
+                                </nav>
+                                <div class="col-6 mb-2">
+                                    <div class="d-flex justify-content-between align-items-center border rounded p-3">
+                                        <div>
+                                            hello roocket
+                                        </div>
+                                        <div>
+                                            <button type="button" class="btn btn-info btn-sm">edit</button>
+                                            <button type="button" class="btn btn-danger btn-sm ms-1">delete</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-6 mb-2">
+                                    <div class="d-flex justify-content-between align-items-center border rounded p-3">
+                                        <div>
+                                            hello roocket
+                                        </div>
+                                        <div>
+                                            <button type="button" class="btn btn-info btn-sm">edit</button>
+                                            <button type="button" class="btn btn-danger btn-sm ms-1">delete</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-6 mb-2">
+                                    <div class="d-flex justify-content-between align-items-center border rounded p-3">
+                                        <div>
+                                            hello roocket
+                                        </div>
+                                        <div>
+                                            <button type="button" class="btn btn-info btn-sm">edit</button>
+                                            <button type="button" class="btn btn-danger btn-sm ms-1">delete</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </main>
+            </div>
+        )
+    }
 }
 
 export default App;
